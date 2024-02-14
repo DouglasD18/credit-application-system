3536ee021ee973e9eac2f0d6f0e7c60b0b17c260
Douglinhas <daguiaralcantara@gmail.com>
2024-02-13 21:11:51 -0300

    feat: create customer getById method

--- src/main/kotlin/credit/application/system/controllers/CustomerController.kt
+++ src/main/kotlin/credit/application/system/controllers/CustomerController.kt
@@ -1,7 +1,11 @@
 package credit.application.system.controllers
 
 import credit.application.system.dtos.CustomerDTO
+import credit.application.system.dtos.CustomerView
+import credit.application.system.entities.Customer
 import credit.application.system.services.impl.CustomerService
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,6 +21,12 @@ class CustomerController(
     fun saveCustomer(@RequestBody customerDTO: CustomerDTO): String {
         val savedCustomer = this.customerService.save(customerDTO.toEntity())
         return "Customer ${savedCustomer.email} saved!"
+    }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        return CustomerView(customer)
     }
 
 }
