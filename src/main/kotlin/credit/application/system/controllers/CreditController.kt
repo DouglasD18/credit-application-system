d072fe674688b258cd556d021ecc302fab31d50a
Douglinhas <daguiaralcantara@gmail.com>
2024-02-17 20:50:39 -0300

    feat: craete get by id method to credit

--- src/main/kotlin/credit/application/system/controllers/CreditController.kt
+++ src/main/kotlin/credit/application/system/controllers/CreditController.kt
@@ -1,12 +1,16 @@
 package credit.application.system.controllers
 
 import credit.application.system.dtos.CreditDTO
+import credit.application.system.dtos.CreditView
 import credit.application.system.entities.Credit
 import credit.application.system.services.impl.CreditService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.stream.Collectors
 
 @RestController
 @RequestMapping("/api/credit")
@@ -18,6 +22,13 @@ class CreditController(
     fun saveCredit(@RequestBody creditDTO: CreditDTO): String {
         val credit: Credit = this.creditService.save(creditDTO.toEntity())
         return "Credit ${credit.creditCode} - Customer ${credit.customer?.email} saved!"
+    }
+
+    @GetMapping
+    fun findByCustomerId(@RequestParam(value = "customerId") customerId: Long): List<CreditView> {
+        return this.creditService.findAllByCustomerId(customerId).stream()
+            .map { credit: Credit -> CreditView(credit) }
+            .collect(Collectors.toList())
     }
 
 }
---
+++ src/main/kotlin/credit/application/system/dtos/CreditView.kt
@@ -0,0 +1,19 @@
+package credit.application.system.dtos
+
+import credit.application.system.entities.Credit
+import java.math.BigDecimal
+import java.util.UUID
+
+data class CreditView(
+    val creditCode: UUID,
+    val creditValue: BigDecimal,
+    val numberOfInstallments: Int
+) {
+
+    constructor(credit: Credit): this(
+        creditCode = credit.creditCode,
+        creditValue = credit.creditValue,
+        numberOfInstallments = credit.numberOfInstallments
+    )
+
+}
