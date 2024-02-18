fafd525ce811e3070bb90b17090cbd3dce1f5421
Douglinhas <daguiaralcantara@gmail.com>
2024-02-17 21:04:30 -0300

    feat: craete get by creditCode method to credit

--- src/main/kotlin/credit/application/system/controllers/CreditController.kt
+++ src/main/kotlin/credit/application/system/controllers/CreditController.kt
@@ -1,15 +1,19 @@
 package credit.application.system.controllers
 
 import credit.application.system.dtos.CreditDTO
+import credit.application.system.dtos.CreditListView
 import credit.application.system.dtos.CreditView
 import credit.application.system.entities.Credit
 import credit.application.system.services.impl.CreditService
+import jakarta.websocket.server.PathParam
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 import java.util.stream.Collectors
 
 @RestController
@@ -25,10 +29,16 @@ class CreditController(
     }
 
     @GetMapping
-    fun findByCustomerId(@RequestParam(value = "customerId") customerId: Long): List<CreditView> {
+    fun findByCustomerId(@RequestParam(value = "customerId") customerId: Long): List<CreditListView> {
         return this.creditService.findAllByCustomerId(customerId).stream()
-            .map { credit: Credit -> CreditView(credit) }
+            .map { credit: Credit -> CreditListView(credit) }
             .collect(Collectors.toList())
+    }
+
+    @GetMapping
+    fun findByCreditCode(@RequestParam(value = "customerId") customerId: Long, @PathVariable creditCode: UUID): CreditView {
+        val credit = this.creditService.findByCreditCode(customerId, creditCode)
+        return CreditView(credit)
     }
 
 }
--- src/main/kotlin/credit/application/system/dtos/CreditView.kt
+++ src/main/kotlin/credit/application/system/dtos/CreditView.kt
@@ -1,19 +1,26 @@
 package credit.application.system.dtos
 
 import credit.application.system.entities.Credit
+import credit.application.system.enummerations.Status
 import java.math.BigDecimal
 import java.util.UUID
 
 data class CreditView(
     val creditCode: UUID,
     val creditValue: BigDecimal,
-    val numberOfInstallments: Int
+    val numberOfInstallments: Int,
+    val status: Status,
+    val emailCustomer: String?,
+    val incomeCustomer: BigDecimal?
 ) {
 
     constructor(credit: Credit): this(
         creditCode = credit.creditCode,
         creditValue = credit.creditValue,
-        numberOfInstallments = credit.numberOfInstallments
+        numberOfInstallments = credit.numberOfInstallments,
+        status = credit.status,
+        emailCustomer = credit.customer?.email,
+        incomeCustomer = credit.customer?.income
     )
 
 }
