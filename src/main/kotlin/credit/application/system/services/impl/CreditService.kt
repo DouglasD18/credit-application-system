f29a2487044012beacc52f40ae7f39df0a153015
Douglinhas <daguiaralcantara@gmail.com>
2024-02-19 12:47:56 -0300

    feat: implement dayFisrtsInstallment validation

--- src/main/kotlin/credit/application/system/services/impl/CreditService.kt
+++ src/main/kotlin/credit/application/system/services/impl/CreditService.kt
@@ -5,6 +5,7 @@ import credit.application.system.exceptions.BusinessException
 import credit.application.system.repositories.CreditRepository
 import credit.application.system.services.ICreditService
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import java.util.*
 
 @Service
@@ -13,6 +14,7 @@ class CreditService(
     private val customerService: CustomerService
 ): ICreditService {
     override fun save(credit: Credit): Credit {
+        this.validDayFirstInstallment(credit.dayFirstInstallment)
         credit.apply {
             customer = customerService.findById(credit.customer?.id!!)
         }
@@ -25,5 +27,10 @@ class CreditService(
     override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
         val credit: Credit = (this.repository.findByCreditCode(creditCode) ?: throw BusinessException("Creditcode $creditCode not found!"))
         return if (credit.customer?.id == customerId) credit else throw IllegalArgumentException("Permission denied")
+    }
+
+    private fun validDayFirstInstallment(dayFirstInstallment: LocalDate): Boolean {
+        return if (dayFirstInstallment.isBefore(LocalDate.now().plusMonths(3))) true
+        else throw BusinessException("Invalid Date")
     }
 }
